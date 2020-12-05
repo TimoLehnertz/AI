9b8e8fecc994dbaf9af97146d6e7b176514a141a
TimoLehnertz <timolehnertz1@gmail.com>
2020-12-05 13:52:21 +0100

    redo

---
+++ src/ai/FullyConnectedLayer.java
@@ -0,0 +1,24 @@
+package ai;
+
+/**
+ * Layer implementation wich connects all own neurons to all neurons in the following layer if not the last layer
+ * 
+ * @author Timo Lehnertz
+ *
+ */
+
+public class FullyConnectedLayer extends Layer {
+
+	@Override
+	public int getInputSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getOutputSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/ai/Layer.java
+++ src/ai/Layer.java
@@ -1,114 +1,31 @@
 package ai;
 
 /**
- * Layer class to represent a layer of neurons in a Network
+ * General abstract layer class for all Artificial neuronal network layers
+ * All layers contained in an ANN get initialized with their next sibling or null in case of beeing last layer
  * 
- * @author timo
+ * All layers have a specific amount of inputs and a specified amount of outputs
+ * what happens inside the layer is beeing defined in the various implementations
+ * 
+ * 
+ * 
+ * @author Timo Lehnertz
  *
  */
-public class Layer {
+public abstract class Layer {
 
-	/**
-	 * The heart of every Layer
-	 */
-	private Neuron[] neurons;
 	
 	/**
-	 * 
-	 * @param size
-	 * @param nextLayer (can be null)
-	 */
-	public Layer(int size, Layer nextLayer) {
-		super();
-		neurons = new Neuron[size];
-		for (int i = 0; i < size; i++) {
-			neurons[i] = new Neuron(nextLayer);
-		}
-	}
-	
-	/**
-	 * get all activations from this layers neurons with sigmoid function aplied
-	 * used for getting the result after an operation from th last layer
-	 */
-	public double[] getResult() {
-		double[] result = new double[neurons.length];
-		for (int i = 0; i < neurons.length; i++) {
-			result[i] = neurons[i].getSigmoidActivation();
-		}
-		return result;
-	}
-	
-	/**
-	 * get the size of this layer
+	 * Return the amount of output signals
+	 * should return the same as getOutputSize() for simple layers
 	 * @return
 	 */
-	protected int getSize() {
-		return neurons.length;
-	}
+	public abstract int getInputSize();
 	
 	/**
-	 * used for initialization of neurons in the previous layer
+	 * Return the amount of input signals
+	 * should return the same as getInputSize() for simple layers
 	 * @return
 	 */
-	protected Connection[] getConnections(){
-		Connection[] connections = new Connection[neurons.length];
-		for (int i = 0; i < connections.length; i++) {
-			connections[i] = new Connection(neurons[i], 0);
-		}
-		return connections;
-	}
-	
-	/**
-	 * fire all neurons in this layer
-	 */
-	protected void fire() {
-		for (Neuron neuron : neurons) {
-			neuron.fire();
-		}
-	}
-	
-	/**
-	 * reset all neurons activation to 0
-	 */
-	protected void reset() {
-		for (Neuron neuron : neurons) {
-			neuron.reset();
-		}
-	}
-
-	/**
-	 * set the activation of all neurons
-	 * used to set the input activations of the first layer
-	 * @param input
-	 */
-	protected void setInput(double[] input) {
-		for (int i = 0; i < input.length; i++) {
-			neurons[i].setActivation(input[i]);
-		}
-	}
-
-	/**
-	 * mutate the connections strength by amount x
-	 * having strength of 0.5 and plaieing mutate 0.5 means having a chance to get a strength between 0 and 1 before sigmoid gets aplied
-	 * @param amount
-	 */
-	protected void mutate(double amount) {
-		for (Neuron neuron : neurons) {
-			neuron.mutate(amount);
-		}
-	}
-
-	/**
-	 * Reset all connections
-	 */
-	protected void resetConnections() {
-		for (Neuron neuron : neurons) {
-			neuron.resetConnections();
-		}
-	}
-	
-	@Override
-	public String toString() {
-		return "Layer w " + neurons.length + " Neurons";
-	}
+	public abstract int getOutputSize();
 }
